adac2c76ddec2bab7cd7139449d0356811ad113c
Ionhedblom <ionysoshedblom@gmail.com>
2020-03-15 14:51:53 +0100

    Added middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // Bring user model
@@ -34,7 +36,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
       
       const avatar = gravatar.url(email, {
@@ -60,8 +62,22 @@ router.post(
       // Save the user (DUUUUH)
       await user.save();
 
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
 
-      res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        // Change to 3600 before deployment
+        { expiresIn: 360000 }, 
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch(err) {
       console.error(err.message);
       res.status(500).send('Server error');
