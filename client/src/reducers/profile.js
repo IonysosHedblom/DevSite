f132a22ea5b63dc98edb15cc68061ee1ba43cbcc
Ionhedblom <ionysoshedblom@gmail.com>
2020-04-18 18:31:03 +0200

    AddExperience and AddEducation added

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from '../actions/types';
 
 // Profile is gonna make a request and get all profile data and store it
 const initialState = {
@@ -14,6 +19,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
