e9543d045852d60c8265407f57b7ff55d028ee12
Ionhedblom <ionysoshedblom@gmail.com>
2020-04-20 13:47:20 +0200

    Added Education and Experience to dashboard

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Experience = ({ experience }) => {
+  return <div></div>;
+};
+
+Experience.propTypes = {};
+
+export default Experience;
